0a3f63d148956780e6b7ce8dcee08cef73707c9a
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-10-06 01:57:40 +0300

    feat(App): add entrypoint

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/* Vue */
+import Vue from 'vue'
+
+/* Plugins */
+import vuetify from './plugins/vuetify'
+
+/* App */
+import App from './App'
+import router from './router'
+import store from './store'
+
+/* Assets */
+import './assets/styles/app.scss'
+
+Vue.config.productionTip = false
+
+new Vue({ router, store, vuetify, render: (h) => h(App) }).$mount('#app')
