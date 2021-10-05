f1ee945dd6a28a94e357251ff735dc0e90a849c1
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-10-06 01:22:16 +0300

    feat(Sidebar): add logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,14 @@
+<script>
+/* Assets */
+import SvgLogo from '@/assets/img/logo.svg'
+
+export default {
+  name: 'Logo',
+
+  components: { SvgLogo }
+}
+</script>
+
+<template>
+  <SvgLogo />
+</template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,39 @@
+<script>
+/* Vuetify */
+import { VNavigationDrawer } from 'vuetify/lib'
+
+/* Icons */
+import { mdiHelpCircle } from '@mdi/js'
+
+/* Components */
+import Logo from './Logo'
+
+export default {
+  name: 'Sidebar',
+
+  components: { Logo, VNavigationDrawer },
+
+  data() {
+    return { mdiHelpCircle, drawer: true }
+  }
+}
+</script>
+
+<template>
+  <v-navigation-drawer v-model="drawer" app class="pt-4 elevation-4" color="secondary" mini-variant stateless>
+    <router-link aria-label="Home" to="/">
+      <div class="sidebar__logo d-flex mx-auto">
+        <Logo />
+      </div>
+    </router-link>
+  </v-navigation-drawer>
+</template>
+
+<style lang="scss">
+.sidebar {
+  &__logo {
+    height: 40px;
+    width: 40px;
+  }
+}
+</style>
