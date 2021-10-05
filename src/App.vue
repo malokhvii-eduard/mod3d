b6b82af8844e4cedec4979c080cd749a069f9acf
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-10-06 01:54:12 +0300

    feat(App): restore theme before create, switch it after state changed

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+/* Vuex */
+import { mapState } from 'vuex'
+
+/* Vuetify */
+import { VApp, VFadeTransition, VMain } from 'vuetify/lib'
+
+/* Components */
+import Sidebar from './components/Sidebar'
+
+export default {
+  name: 'App',
+
+  components: { Sidebar, VApp, VFadeTransition, VMain },
+
+  computed: {
+    ...mapState('app', ['isDark'])
+  },
+
+  watch: {
+    isDark(value) {
+      this.$vuetify.theme.dark = value
+    }
+  },
+
+  beforeCreate() {
+    this.$store.dispatch('app/restoreLastTheme')
+  }
+}
+</script>
+
+<template>
+  <v-app>
+    <Sidebar />
+
+    <v-main>
+      <v-fade-transition mode="out-in">
+        <router-view />
+      </v-fade-transition>
+    </v-main>
+  </v-app>
+</template>
