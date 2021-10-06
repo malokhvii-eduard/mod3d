877c3ec9fe41cfc4981f0b0da46267f818dceeec
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-10-06 03:13:46 +0300

    feat(views/NotFound): add empty

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 /* Views */
-import Home from '@/views/Home'
 import About from '@/views/About'
+import Home from '@/views/Home'
+import NotFound from '@/views/NotFound'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', name: 'home', component: Home },
-  { path: '/about', name: 'about', component: About }
+  { path: '/about', name: 'about', component: About },
+  { path: '/404', name: '404', component: NotFound },
+  { path: '*', redirect: '404' }
 ]
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<template>
+  <section id="404" />
+</template>
