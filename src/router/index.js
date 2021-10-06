c392f48bdc9168efc4007fae83b8d13752e6e058
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-10-06 03:12:10 +0300

    feat(views/About): add empty

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,14 @@ import VueRouter from 'vue-router'
 
 /* Views */
 import Home from '@/views/Home'
+import About from '@/views/About'
 
 Vue.use(VueRouter)
 
-const routes = [{ path: '/', name: 'home', component: Home }]
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/about', name: 'about', component: About }
+]
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<template>
+  <section id="about" />
+</template>
